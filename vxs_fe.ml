e9630dc190e27accf04b0d2c00e51b82799d87ef
Jerome Maloberti <jerome.maloberti@citrix.com>
2012-12-17 10:30:24 +0000

    Use cmdliner for the command-line.
    Signed-off-by: Jerome Maloberti <jerome.maloberti@citrix.com>

--- vxs_fe.ml
+++ vxs_fe.ml
@@ -17,53 +17,67 @@ open Lwt
 
 module X = Xen_api_lwt_unix
 open Cohttp_lwt_unix
+module Cli = Cmdliner
 
-let host = ref ""
-let uri = ref "http://127.0.0.1/"
-let username = ref "root"
-let password = ref "password"
-let anon = ref []
+type common_opts = { host_: string; username_: string option; password_: string option}
 
-let consume_arg () =
-	let arg = List.hd !anon in
-	anon := List.tl (!anon);
-	arg
+let opt_str = function None -> "None" |	Some v -> v
 
-let test host_config rpc uuid = 
-	lwt session_id = X.Session.login_with_password rpc !username !password "1.1" in
-  lwt n = Vxs.submit_rpc host_config session_id uuid "ls /" in
-  lwt result = Vxs.get_response host_config session_id uuid n in
-  Printf.printf "%s\n%!" result;
-  Lwt.return ()
+let config copts =
+	Host.({host = copts.host_;
+         username = opt_str copts.username_;
+         password = opt_str copts.password_})
 
-let main () =
-	let op = consume_arg () in
-	let host_config = Host.({host = !host; username = !username; password = !password}) in
-	let uri = Printf.sprintf "http://%s/" host_config.Host.host in
+let make_rpc copts =
+	let uri = Printf.sprintf "http://%s/" copts.host_ in
   let rpc = X.make uri in
-	match op with
-	| "install" ->
-			lwt vm_uuid = Xs_ops.create_xenserver_template host_config "trunk-ring3" in
-      Printf.printf "%s\n" vm_uuid;
-      return ()				
-  | "addrpm" ->
-			let uuid = consume_arg () in
-			let rpm = consume_arg () in
-			lwt session_id = X.Session.login_with_password rpc !username !password "1.1" in
-      lwt () = Vxs.add_rpm host_config session_id uuid rpm in
-			return ()
-	| "rpc" ->
-			let uuid = consume_arg () in
-			let script = consume_arg () in 
-			lwt session_id = X.Session.login_with_password rpc !username !password "1.1" in
- 			lwt n = Vxs.submit_rpc host_config session_id uuid "ls /" in
-      lwt result = Vxs.get_response host_config session_id uuid n in
-      Printf.printf "%s\n%!" result;
+  rpc
+
+let install copts branch nofakev6d =
+  let host_config = config copts in
+  let branch = opt_str branch in
+  let aux () =
+	  lwt vm_uuid = Xs_ops.create_xenserver_template host_config branch in
+    Printf.printf "%s\n" vm_uuid;
+    if not nofakev6d then begin
+        let rpc = make_rpc copts in
+        let rpm = Printf.sprintf "/usr/groups/admin/web/www.uk.xensource.com/html/carbon/%s/latest/xe-phase-1/v6-test.rpm" branch in
+        lwt session_id = X.Session.login_with_password rpc
+          (opt_str copts.username_) (opt_str copts.password_) "1.1" in
+        lwt () = Vxs.add_rpm host_config session_id vm_uuid rpm in
+        return ()
+    end else
       return ()
-  | _ -> 
-			Printf.printf "Unknown operation";
-			return ()
-          
+  in
+	Lwt_main.run (aux ())
+
+let add_rpms copts uuid rpms =
+  Printf.printf "add-rpms %s %s\n" uuid (String.concat ", " rpms);
+  let host_config = config copts in
+  let rpc = make_rpc copts in
+  let aux () =
+    lwt session_id = X.Session.login_with_password rpc
+      (opt_str copts.username_) (opt_str copts.password_) "1.1" in
+    lwt () = Lwt_list.iter_s
+      (fun rpm -> Vxs.add_rpm host_config session_id uuid rpm) rpms in
+    return ()
+  in
+  Lwt_main.run (aux ())
+
+let call_rpc copts uuid script =
+  Printf.printf "call-rpc %s %s\n" uuid script;
+  let host_config = config copts in
+  let rpc = make_rpc copts in
+  let aux () =
+	  lwt session_id = X.Session.login_with_password rpc
+      (opt_str copts.username_) (opt_str copts.password_) "1.1" in
+ 	  lwt n = Vxs.submit_rpc host_config session_id uuid "ls /" in
+    lwt result = Vxs.get_response host_config session_id uuid n in
+    Printf.printf "%s\n%!" result;
+    return ()
+  in
+  Lwt_main.run (aux ())
+
 (*	let uri = Printf.sprintf "http://%s/" !host in
         let rpc = make uri in
         lwt session_id = Session.login_with_password rpc !username !password "1.0" in
@@ -77,16 +91,108 @@ let main () =
         finally
                 Session.logout rpc session_id*)
 
-let _ =
-	let op = ref "install" in
-  Arg.parse [
-      "-host", Arg.Set_string host, (Printf.sprintf "Hostname of server to connect to (default %s)" !host);
-      "-u", Arg.Set_string username, (Printf.sprintf "Username to log in with (default %s)" !username);
-      "-pw", Arg.Set_string password, (Printf.sprintf "Password to log in with (default %s)" !password);
-  ] (fun x -> anon := x :: !anon)
-      "VXS toolkit";
-	anon := List.rev !anon;
-	
-  Lwt_main.run (main ())
+let help common_opts man_format cmds topic = match topic with
+| None -> `Help (`Pager, None) (* help about the program. *)
+| Some topic ->
+    let topics = "topics" :: "patterns" :: "environment" :: cmds in
+    let conv, _ = Cli.Arg.enum (List.rev_map (fun s -> (s, s)) topics) in
+    match conv topic with
+    | `Error e -> `Error (false, e)
+    | `Ok t when t = "topics" -> List.iter print_endline topics; `Ok ()
+    | `Ok t when List.mem t cmds -> `Help (man_format, Some t)
+    | `Ok t ->
+        let page = (topic, 7, "", "", ""), [`S topic; `P "Say something";] in
+        `Ok (Cli.Manpage.print man_format Format.std_formatter page)
 
+let common_opts_sect = "COMMON OPTIONS"
+let help_secs = [
+ `S common_opts_sect;
+ `P "These options are common to all commands.";
+ `S "MORE HELP";
+ `P "Use `$(mname) $(i,COMMAND) --help' for help on a single command.";`Noblank;]
 
+(* Options common to all commands *)
+
+let common_opts host_ username_ password_ = { host_; username_; password_ }
+
+let common_opts_t =
+  let docs = common_opts_sect in
+  let host =
+    let doc = "Hostname to connect to." in
+    Cli.Arg.(required & pos 0 (some string) None & info [] ~docs ~doc ~docv:"HOST")
+  in
+  let user =
+    let doc = "Username to log in with." in
+    Cli.Arg.(value & opt (some string) (Some "root") & info ["u"; "user"] ~docs ~doc)
+  in
+  let pw =
+    let doc = "Password to log in with." in
+    Cli.Arg.(value & opt (some string) (Some "xenroot") & info ["p"; "password"] ~docs ~doc)
+  in
+  Cli.Term.(pure common_opts $ host $ user $ pw)
+
+(* Commands *)
+let add_rpms_cmd =
+  let docs = common_opts_sect in
+  let uuid =
+    let doc = "UUID of the VM." in
+    Cli.Arg.(required & pos 1 (some string) None & info [] ~docs ~doc ~docv:"UUID")
+  in
+  let rpms =
+    let doc = "RPM files to copy to the VM." in
+    Cli.Arg.(non_empty & pos_right 1 non_dir_file [] & info [] ~docv:"RPMS" ~doc ~docs)
+  in
+  let doc = "Copy and install RPM files in a VM." in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Copy and install RPM files in a VM."] @ help_secs
+  in
+  Cli.Term.(pure add_rpms $ common_opts_t $ uuid $ rpms),
+  Cli.Term.info "add-rpms" ~sdocs:common_opts_sect ~doc ~man
+
+let create_template_cmd =
+  let branch =
+    let doc = "Branch to install as a template." in
+    Cli.Arg.(value & opt (some string) (Some "trunk-ring3") & info ["b"; "branch"]
+                 ~docv:"BRANCH" ~doc)
+  in
+  let nov6d =
+    let doc = "Do not install the fake v6d." in
+    Cli.Arg.(value & flag & info ["n"; "nofakev6d"] ~doc)
+  in
+  let doc = "install a Virtual Xen Server Template" in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Install a Virtual Xen Server as a Template"] @ help_secs
+  in
+  Cli.Term.(pure install $ common_opts_t $ branch $ nov6d),
+  Cli.Term.info "create-template" ~sdocs:common_opts_sect ~doc ~man
+
+let call_rpc_cmd =
+  let docs = common_opts_sect in
+  let uuid =
+    let doc = "UUID of the VM." in
+    Cli.Arg.(required & pos 1 (some string) None & info [] ~docs ~doc ~docv:"UUID")
+  in
+  let script =
+    let doc = "Script to execute on the VM." in
+    Cli.Arg.(required & pos 2 (some non_dir_file) None & info [] ~docv:"SCRIPT" ~doc ~docs)
+  in
+  let doc = "Execute a script on a VM." in
+  let man = [
+    `S "DESCRIPTION";
+    `P "Execute a script on a VM."] @ help_secs
+  in
+  Cli.Term.(pure call_rpc $ common_opts_t $ uuid $ script),
+  Cli.Term.info "call-rpc" ~sdocs:common_opts_sect ~doc ~man
+
+let default_cmd =
+  let doc = "Virtual XenServer Management Toolkit" in
+  let man = help_secs in
+  Cli.Term.(ret (pure (fun _ -> `Help (`Pager, None)) $ common_opts_t)),
+  Cli.Term.info "vxs_fe" ~version:"0.2" ~sdocs:common_opts_sect ~doc ~man
+
+let cmds = [create_template_cmd; add_rpms_cmd; call_rpc_cmd]
+
+let () = match Cli.Term.eval_choice default_cmd cmds with
+  | `Error _ -> exit 1 | _ -> exit 0
