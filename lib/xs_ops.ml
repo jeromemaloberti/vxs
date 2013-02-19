5a4f2a6496eca96a859a16189c4e0486e4d098b0
Jon Ludlam <jonathan.ludlam@eu.citrix.com>
2013-02-19 15:36:58 +0000

    Add the ability to install from main.iso rather than from pxe.
    Note this requires a very recent build of Sarasota

--- lib/xs_ops.ml
+++ lib/xs_ops.ml
@@ -23,6 +23,10 @@ open Host
 
 open CamlTemplate.Model
 
+type installty = 
+	| Pxe of string (* branch name *)
+	| Mainiso of string (* iso name *)
+
 let pxedir = "/usr/groups/netboot/pxelinux.cfg"
 
 (* API:
@@ -34,7 +38,7 @@ let pxedir = "/usr/groups/netboot/pxelinux.cfg"
  *)
 
 type vxs_template_config = {
-	  branch : string;              (* e.g. trunk-ring3 *)
+	  ty : installty;              (* e.g. Pxe "trunk-ring3" or Mainiso "main.iso" *)
     
 	  vm_uuid : string;                  (* VM uuid *)
 	  vxs_root_password : string; 
@@ -56,8 +60,7 @@ type vxs_pool_config = {
 
 let create_hash host vxs =
 	let h = Hashtbl.create 10 in
-	let l = ["branch",vxs.branch;
-			     "host",host.host;
+	let l = [    "host",host.host;
 			     "username",host.username;
 			     "password",host.password;
 			     "vm_uuid",vxs.vm_uuid;
@@ -68,7 +71,15 @@ let create_hash host vxs =
 			     "firstboot_uuid",vxs.firstboot.Blob.u;
 			     "id_dsa_uuid",vxs.id_dsa.Blob.u;
 			     "answerfile_uuid",vxs.answerfile.Blob.u;] in
-	List.iter (fun (x,y) -> Hashtbl.add h x (Tstr y)) l;
+	let extra = match vxs.ty with
+		| Pxe branch -> 
+			[ "branch",branch;
+			  "sourcetype","url" ]
+		| Mainiso isoname ->
+			[ "branch","";
+			  "sourcetype","local"] 
+	in
+	List.iter (fun (x,y) -> Hashtbl.add h x (Tstr y)) (l @ extra);
 	h
       
 let cache = CamlTemplate.Cache.create ()
@@ -86,6 +97,13 @@ let get_initscript = get "install/initscript"
 let get_post_install = get "install/post_install"
 let get_answerfile = get "install/answerfile"
 let get_veryfirstboot = get "install/veryfirstboot"
+let get_linux_cmdline = get "install/linux_cmdline"
+
+let meg = Int64.mul 1024L 1024L
+let gig = Int64.mul 1024L meg
+let g2 = Int64.mul gig 2L
+let g40 = Int64.mul gig 40L
+let m4 = Int64.mul meg 4L
 
 let exn_to_string = function
   | Api_errors.Server_error(code, params) ->
@@ -102,7 +120,7 @@ let check_pxe_dir () =
 
 
 
-let create_xenserver_template host branch =
+let create_xenserver_template host ty =
 	lwt () = check_pxe_dir () in
 	let uri = Printf.sprintf "http://%s/" host.host in
     let rpc = X.make uri in
@@ -110,16 +128,10 @@ let create_xenserver_template host branch =
     lwt templates = X.VM.get_all_records_where ~rpc ~session_id ~expr:"field \"name__label\" = \"Other install media\"" in
     let (template,_) = List.hd templates in
 	Printf.printf "Found template ref: %s\n" template;
-	lwt vm = X.VM.clone ~rpc ~session_id ~vm:template ~new_name:(Printf.sprintf "%s-unknown" branch) in
+	lwt vm = X.VM.clone ~rpc ~session_id ~vm:template ~new_name:"xenserver-unknown" in
     lwt vm_uuid = X.VM.get_uuid ~rpc ~session_id ~self:vm in
     lwt () = X.VM.provision ~rpc ~session_id ~vm in
-    let gig = Int64.mul 1024L (Int64.mul 1024L 1024L) in
-	let g2 = Int64.mul gig 2L in
-	let g40 = Int64.mul gig 40L in
     lwt () = X.VM.set_memory_limits ~rpc ~session_id ~self:vm ~static_min:g2 ~static_max:g2 ~dynamic_min:g2 ~dynamic_max:g2 in
-
-    lwt () = X.VM.remove_from_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" in
-    lwt () = X.VM.add_to_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" ~value:"ncd" in
 
     lwt nets = X.Network.get_all_records_where ~rpc ~session_id ~expr:"field \"bridge\" = \"xenbr0\"" in
     let (network,_) = List.hd nets in
@@ -138,7 +150,7 @@ let create_xenserver_template host branch =
     lwt id_dsa = Blob.add_blob rpc session_id vm "id_dsa" in
 
     let vxs_template_config = {
-		branch;
+		ty;
 		vm_uuid;
         vxs_root_password = host.password;
 		post_install;
@@ -170,20 +182,41 @@ let create_xenserver_template host branch =
       Blob.put_blob host session_id id_dsa id_dsa_string
     end else Lwt.return () in   
 
+    let linux_cmdline = get_linux_cmdline host vxs_template_config in
+
     let pxe_path = Printf.sprintf "%s/%s" pxedir vm_uuid in
 	let pxe_config = get_pxe_config host vxs_template_config in
-	
-	lwt fd = Lwt_unix.openfile pxe_path [Lwt_unix.O_WRONLY; Lwt_unix.O_CREAT] 0o666 in
-    let c = Lwt_io.(of_fd output fd) in
 
-    lwt () = Lwt_io.write c pxe_config in
-    lwt () = Lwt_io.close c in
+	lwt () = begin match vxs_template_config.ty with
+		| Pxe branch ->
+			lwt fd = Lwt_unix.openfile pxe_path [Lwt_unix.O_WRONLY; Lwt_unix.O_CREAT] 0o666 in
+            let c = Lwt_io.(of_fd output fd) in
+	
+			lwt () = Lwt_io.write c pxe_config in
+            lwt () = Lwt_io.close c in
+
+            lwt () = X.VM.remove_from_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" in
+            lwt () = X.VM.add_to_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" ~value:"ncd" in
+
+            Lwt.return () 
+        | Mainiso isoname ->
+			lwt isos = X.VDI.get_by_name_label ~rpc ~session_id ~label:isoname in
+            let iso = List.hd isos in
+            lwt vbd = X.VBD.create ~rpc ~session_id ~vDI:iso ~vM:vm ~userdevice:"3" ~bootable:true ~mode:`RO ~_type:`CD ~unpluggable:true ~empty:false ~other_config:[] ~qos_algorithm_type:"" ~qos_algorithm_params:[] in
+            lwt vdi2 = X.VDI.create ~rpc ~session_id ~sR:default_sr ~name_label:"Autoinstall disk" ~name_description:"" ~virtual_size:m4 ~_type:`user ~sharable:false ~read_only:false ~other_config:[] ~xenstore_data:[] ~sm_config:[] ~tags:[] in
+            lwt vbd = X.VBD.create ~rpc ~session_id ~vDI:vdi2 ~vM:vm ~userdevice:"1" ~bootable:false ~mode:`RW ~_type:`Disk ~unpluggable:false ~empty:false ~other_config:["owner",""] ~qos_algorithm_type:"" ~qos_algorithm_params:[] in
+            let fname = "/tmp/autoinstalldisk" in
+            lwt () = Utils.create_vfat_disk fname "autoinstall" in
+            lwt () = Utils.copy_to_vfat fname "cmdline" linux_cmdline in
+            lwt contents = Utils.read_file fname  in
+            lwt () = Utils.put_disk host session_id vdi2 contents in
+            Lwt.return ()
+    end in
 
     lwt () = X.VM.start ~rpc ~session_id ~vm ~start_paused:false ~force:false in
-
     lwt () = X.VM.remove_from_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" in
     lwt () = X.VM.add_to_HVM_boot_params ~rpc ~session_id ~self:vm ~key:"order" ~value:"cd" in
- 
+
     let rec wait token =
 		lwt events = X.Event.from ~rpc ~session_id ~classes:[Printf.sprintf "vm/%s" vm] ~token ~timeout:1.0 in
 	    let ef = Event_types.event_from_of_rpc events in 
