423daef0a8989af28139605e4b89d4007fd55397
Jerome Maloberti <jerome.maloberti@citrix.com>
2013-05-02 16:36:02 +0100

    Fix indentation.
    Signed-off-by: Jerome Maloberti <jerome.maloberti@citrix.com>

--- lib/blob.ml
+++ lib/blob.ml
@@ -18,37 +18,37 @@ open Cohttp_lwt_unix
 module X=Xen_api_lwt_unix
 
 type t = {
-	  r : string;
-	  u : string;
+  r : string;
+  u : string;
 }
 
 let put_blob host_config session_id blob value =
-	match Body.body_of_string value with 
-	| Some v -> 
-			let uri = Printf.sprintf "http://%s/blob?session_id=%s&ref=%s" host_config.Host.host session_id blob.r in
-			Utils.http_put uri v
-	| None -> 
-			Lwt.return ()
-
+  match Body.body_of_string value with 
+  | Some v -> 
+    let uri = Printf.sprintf "http://%s/blob?session_id=%s&ref=%s" host_config.Host.host session_id blob.r in
+    Utils.http_put uri v
+  | None -> 
+    Lwt.return ()
+      
 let add_blob rpc session_id vm name =
-	lwt blob = X.VM.create_new_blob ~rpc ~session_id ~vm ~name ~mime_type:"" ~public:true in
+  lwt blob = X.VM.create_new_blob ~rpc ~session_id ~vm ~name ~mime_type:"" ~public:true in
   lwt uuid = X.Blob.get_uuid ~rpc ~session_id ~self:blob in
   return { r = blob; u = uuid; }
-      
+    
 let get_blob host_config session_id blob = 
-	let uri = Printf.sprintf "http://%s/blob?session_id=%s&ref=%s" host_config.Host.host session_id blob.r in
+  let uri = Printf.sprintf "http://%s/blob?session_id=%s&ref=%s" host_config.Host.host session_id blob.r in
   Utils.http_get uri
-      
+    
 let add_blob_with_content host_config rpc session_id vm name value =
-	lwt blob = add_blob rpc session_id vm name in
+  lwt blob = add_blob rpc session_id vm name in
   lwt () = put_blob host_config session_id blob value in
   return blob
-      
+    
 let of_uuid rpc session_id u =
-	lwt r = X.Blob.get_by_uuid rpc session_id u in
-	return { r; u }
-      
+  lwt r = X.Blob.get_by_uuid rpc session_id u in
+  return { r; u }
+    
 let of_ref rpc session_id r =
-	lwt u = X.Blob.get_uuid rpc session_id r in
-	return { r; u }
+  lwt u = X.Blob.get_uuid rpc session_id r in
+  return { r; u }
       
